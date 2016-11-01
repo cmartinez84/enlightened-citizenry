641c6a5a77f9b6b02affe7a4a235bb04c11c696b
chris martinez and evan stewart <chris@>
2016-11-01 09:32:54 -0700

    add api key

---
+++ app/routes/results.js
@@ -0,0 +1,14 @@
+import config from '../config/environment';
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params) {
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' +params.zip;
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      console.log(responseJSON);
+      return responseJSON.results;
+
+    });
+  }
+});
